fb79537d8fe2e7205f22bf146821479a6892d2a3
Raphael ezema <ezemaraphael94@gmail.com>
2025-01-06 13:27:14 +0100

    Production2

--- app.py
+++ app.py
@@ -14,6 +14,17 @@ class Todo(db.Model):
         return '<Task %r>' % self.id
 
 
+# Initialize the database manually
+@app.route('/initdb')
+def initdb():
+    try:
+        db.create_all()
+        return "Database initialized successfully!"
+    except Exception as e:
+        return f"An error occurred: {e}"
+
+        
+
 @app.route('/',methods=['POST','GET'])
 def index():
     if request.method == 'POST':
