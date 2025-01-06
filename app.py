2eb0f054cea9316b5c9d01d927b97d2c8bc4ad59
Raphael ezema <ezemaraphael94@gmail.com>
2025-01-06 13:16:45 +0100

    Production

--- app.py
+++ app.py
@@ -63,6 +63,6 @@ def update(id):
 
 
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
+    # with app.app_context():
+    #     db.create_all()
     app.run(debug=True)
